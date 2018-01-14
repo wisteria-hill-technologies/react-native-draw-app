faf282008658ad8a4ef4da62cc0af83a6df055eb
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2018-01-14 10:51:56 +0000

    adds clear, rewind, save buttons to do all the functionalities. lists savedDrawings to component state.

---
+++ App.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native'
+import RNDraw from './rn-draw-additional'
+import uuidv1 from 'uuid/v1'
+import {Svg} from './rn-draw-additional/src/config'
+const {
+  G, 
+  Surface, 
+  Path
+} = Svg
+
+export default class App extends React.Component {
+
+  state={
+    svgId: null,
+    currentSVGInfo: null,
+    svgObjects:[]
+  }
+
+  componentDidMount(){
+    this.setState({
+      svgId: uuidv1()
+    })
+  }
+
+  rewind=()=>{
+    this._undo()
+  }
+
+  clear=()=>{
+    this._clear()
+  }
+
+  updateSVGInfo=(svgObj)=>{
+    this.setState({
+      currentSVGInfo: svgObj
+    })
+  }
+
+  save=()=>{
+    const newSVGObjToSave = {
+      id: this.state.svgId,
+      ...this.state.currentSVGInfo
+    }
+    this.setState({
+      svgObjects: [...this.state.svgObjects, newSVGObjToSave]
+    },()=>{
+      this.clear()
+      this.setState({
+        svgId: uuidv1()
+      })
+    })
+  }
+
+  render() {
+    const { svgObjects } = this.state
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+        <Text>React Native Draw</Text>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            onPress={this.clear}
+            style={[styles.btn, styles.btnRed]}
+          >
+            <Text style={styles.btnText}>Clear</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.rewind}
+            style={[styles.btn, styles.btnOrange]}
+          >
+            <Text style={styles.btnText}>Rewind</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.save}
+            style={[styles.btn, styles.btnGreen]}
+          >
+            <Text style={styles.btnText}>Save</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.imageFrame}>
+          <Image
+            style={styles.backgroundImg}
+            source={(require('./whiteboard.jpg'))}
+          />
+          <RNDraw
+            containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+            rewind={(undo) => {this._undo = undo}}
+            clear={(clear) => {this._clear = clear}}
+            updateSVGInfo={(svgObj)=>{this.updateSVGInfo(svgObj)}}
+            color={'#000000'}
+            strokeWidth={4}
+            svgId={this.state.svgId}
+          />
+        </View>
+
+        <View>
+          <Text>Saved Drawings</Text>
+            {(svgObjects.length > 0)&&svgObjects.map((svgObj, index)=>{
+              return (
+                <View
+                  key={index}
+                  style={styles.imageFrame}
+                >
+                  <Image
+                    style={styles.backgroundImg}
+                    source={(require('./whiteboard.jpg'))}
+                  />
+                  <Svg style={styles.drawSurface}>
+                    <G>
+                      {svgObj.previousStrokes}
+                      <Path
+                        // key={this.state.tracker}
+                        d={svgObj.d}
+                        stroke={ svgObj.stroke || "#000000"}
+                        strokeWidth={svgObj.storkeWidth || 4}
+                        fill={svgObj.fill}
+                      />
+                    </G>
+                  </Svg>
+                </View>
+              )
+            })}
+        </View>
+
+      </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 10
+  },
+  imageFrame:{
+    height: 350,
+    width:350,
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  btnContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btn: {
+    margin: 20,
+    padding: 10
+  },
+  btnGreen:{
+    backgroundColor: 'green',
+  },
+  btnOrange:{
+    backgroundColor: 'orange',
+  },
+  btnRed: {
+    backgroundColor: 'red',
+  },
+  btnText: {
+    textAlign: 'center'
+  },
+  drawContainer: {
+    flex: 1,
+    display: 'flex',
+  },
+  svgContainer: {
+    flex: 1,
+  },
+  drawSurface: {
+    flex: 1,
+  },
+  backgroundImg: {
+    width: '100%', height: '100%', position: 'absolute'
+  }
+});
